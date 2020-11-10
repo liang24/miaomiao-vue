6cf24b17829c22594fcb48ab9d719aad27798ced
liang24 <380431715@qq.com>
2020-11-10 16:00:12 +0800

    add setData

--- src/components/City/index.vue
+++ src/components/City/index.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="city_body">
-    <div class="city_list">
+    <!-- <div class="city_list">
       <div class="city_hot">
         <h2>热门城市</h2>
         <ul class="clearfix">
@@ -24,51 +24,6 @@
             <li>安阳</li>
           </ul>
         </div>
-        <div>
-          <h2>B</h2>
-          <ul>
-            <li>北京</li>
-            <li>保定</li>
-            <li>蚌埠</li>
-            <li>包头</li>
-          </ul>
-        </div>
-        <div>
-          <h2>A</h2>
-          <ul>
-            <li>阿拉善盟</li>
-            <li>鞍山</li>
-            <li>安庆</li>
-            <li>安阳</li>
-          </ul>
-        </div>
-        <div>
-          <h2>B</h2>
-          <ul>
-            <li>北京</li>
-            <li>保定</li>
-            <li>蚌埠</li>
-            <li>包头</li>
-          </ul>
-        </div>
-        <div>
-          <h2>A</h2>
-          <ul>
-            <li>阿拉善盟</li>
-            <li>鞍山</li>
-            <li>安庆</li>
-            <li>安阳</li>
-          </ul>
-        </div>
-        <div>
-          <h2>B</h2>
-          <ul>
-            <li>北京</li>
-            <li>保定</li>
-            <li>蚌埠</li>
-            <li>包头</li>
-          </ul>
-        </div>
       </div>
     </div>
     <div class="city_index">
@@ -79,6 +34,35 @@
         <li>D</li>
         <li>E</li>
       </ul>
+    </div> -->
+
+    <div class="city_list">
+      <div class="city_hot">
+        <h2>热门城市</h2>
+        <ul class="clearfix">
+          <li v-for="item in hotList" :key="item.id">{{ item.nm }}</li>
+        </ul>
+      </div>
+      <div class="city_sort" ref="city_sort">
+        <div v-for="item in cityList" :key="item.index">
+          <h2>{{ item.index }}</h2>
+          <ul>
+            <li v-for="c in item.list" :key="c.id">{{ c.nm }}</li>
+          </ul>
+        </div>
+      </div>
+    </div>
+
+    <div class="city_index">
+      <ul>
+        <li
+          v-for="(item, index) in cityList"
+          :key="item.index"
+          @touchstart="handleToIndex(index)"
+        >
+          {{ item.index }}
+        </li>
+      </ul>
     </div>
   </div>
 </template>
@@ -86,6 +70,81 @@
 <script>
 export default {
   name: "City",
+  data() {
+    return {
+      cityList: [],
+      hotList: [],
+    };
+  },
+  mounted() {
+    this.axios.get("/api/cityList").then((res) => {
+      // console.log(res);
+      var msg = res.data.msg;
+      if (msg === "ok") {
+        var data = res.data.data.cities;
+        // console.log(data);
+        // [{ index : 'A', list : [{ nm : '', id : 123 }]}]
+
+        var { cityList, hotList } = this.formatCityList(data);
+        this.cityList = cityList;
+        this.hotList = hotList;
+      }
+    });
+  },
+  methods: {
+    formatCityList(cities) {
+      var cityList = [];
+      var hotList = [];
+
+      for (var i = 0; i < cities.length; i++) {
+        var firstLetter = cities[i].py.substring(0, 1).toUpperCase();
+        var item = getItem(firstLetter);
+
+        if (item) {
+          // 已有index
+          item.list.push({ nm: cities[i].nm, id: cities[i].id });
+        } else {
+          // 新添加index
+          cityList.push({
+            index: firstLetter,
+            list: [{ nm: cities[i].nm, id: cities[i].id }],
+          });
+        }
+
+        if (cities[i].isHot) {
+          hotList.push({ nm: cities[i].nm, id: cities[i].id });
+        }
+      }
+
+      cityList.sort((n1, n2) => {
+        if (n1.index > n2.index) {
+          return 1;
+        } else if (n1.index < n2.index) {
+          return -1;
+        } else {
+          return 0;
+        }
+      });
+
+      function getItem(firstLetter) {
+        for (var i = 0; i < cityList.length; i++) {
+          if (cityList[i].index == firstLetter) {
+            return cityList[i];
+          }
+        }
+        return false;
+      }
+
+      return {
+        cityList,
+        hotList,
+      };
+    },
+    handleToIndex(index) {
+      var h2 = this.$refs.city_sort.getElementsByTagName("h2");
+      this.$refs.city_sort.parentNode.scrollTop = h2[index].offsetTop;
+    },
+  },
 };
 </script>
 
--- src/main.js
+++ src/main.js
@@ -1,12 +1,19 @@
-import Vue from 'vue'
-import App from './App.vue'
-import router from './routers'
-import store from './stores'
+import Vue from "vue";
+import App from "./App.vue";
+import router from "./routers";
+import store from "./stores";
 
-Vue.config.productionTip = false
+import axios from "axios";
+Vue.prototype.axios = axios;
+
+Vue.filter("setWH", (url, arg) => {
+  return url.replace(/w\.h/, arg);
+});
+
+Vue.config.productionTip = false;
 
 new Vue({
   router,
   store,
-  render: h => h(App)
-}).$mount('#app')
+  render: (h) => h(App),
+}).$mount("#app");
